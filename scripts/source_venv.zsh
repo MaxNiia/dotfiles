874be4df36a3559b3f7ce196805e85a2ffd00611
Max Niia <max.niia@external.epiroc.com>
2023-04-04 07:40:27 +0200

    Add auto complete to source_venv

--- scripts/source_venv.zsh
+++ scripts/source_venv.zsh
@@ -1,22 +1,31 @@
 #!/bin/zsh
 
-function source_venv()
+# Set the directory path where the virtual environments are stored
+local venv_dir="$HOME/venvs"
+
+# Define a function to provide completion options based on the directories in the venv folder
+_venv_completion() {
+    local file
+    for file in ~/venvs/*; do
+        # Skip entries that are not a directory.
+        [[ -d $file ]] || continue
+
+        # Add the file without the ~/venvs/ prefix to the list of 
+        # autocomplete suggestions.
+        COMPREPLY+=( $(basename "$file") )
+    done
+}
+
+# Set the completion function for the "source_venv" command
+complete -F _venv_completion source_venv
+
+source_venv()
 {
-    # Set the directory path where the virtual environments are stored
-    local venv_dir="$HOME/venvs"
-
-    ls $venv_dir
-
-    # Prompt user for the virtual environment name
-    echo "Enter the name of the virtual environment to activate:"
-    read venv_name
-
-    # Check if the virtual environment exists
-    if [ -d "$venv_dir/$venv_name" ]; then
-        # Activate the virtual environment
-        source "$venv_dir/$venv_name/bin/activate"
-        echo "Activated virtual environment: $venv_name"
+    local venv_name="$1"
+    if [ -d "${venv_dir}/${venv_name}" ]; then
+        source "${venv_dir}/${venv_name}/bin/activate"
+        echo "Activated virtual environment: ${venv_name}"
     else
-        echo "Virtual environment $venv_name not found in $venv_dir"
+        echo "Virtual environment ${venv_name} not found in ${venv_dir}"
     fi
 }
