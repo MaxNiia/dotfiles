72bdcb9cfbb66b7289c435f967d25d9cacb4338d
Max Niia <muxinatorn@gmail.com>
2023-07-19 17:19:53 +0200

    Add history options

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,5 +1,9 @@
 #!/usr/env zsh
 
+HISTFILE=~/.zsh_history
+HISTSIZE=1000
+SAVEHIST=1000
+
 # TODO: Move to powerlevel10k
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
