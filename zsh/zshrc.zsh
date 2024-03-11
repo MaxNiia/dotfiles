418cfcb20ee8fcc2eed86a027874c125adc131a4
Max Niia <muxinatorn@gmail.com>
2024-03-11 20:54:10 +0100

    feat(wezterm): Add wezterm support for underlines

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -9,6 +9,11 @@ setopt APPEND_HISTORY
 setopt EXTENDED_HISTORY
 setopt INC_APPEND_HISTORY
 
+if grep -q "microsoft" /proc/version &>/dev/null; then
+   alias nvim="env TERM=wezterm nvim"
+fi
+
+
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.
