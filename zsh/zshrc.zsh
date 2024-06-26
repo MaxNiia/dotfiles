1f4d93c7d1d1a435665b2bf556aa68d9c3d5b139
MaxNiia <muxinatorn@gmail.com>
2024-06-26 06:06:24 +0200

    fix: fzf latte options

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -114,7 +114,7 @@ else
    # FZF
    export FZF_DEFAULT_OPTS=" \
    --color=bg+:#ccd0da,bg:#eff1f5,spinner:#dc8a78,hl:#d20f39 \
-   --color=fg:#4c4f69,header:#d20f39,info:#8839ef,pointer:#dc8a78 \G
+   --color=fg:#4c4f69,header:#d20f39,info:#8839ef,pointer:#dc8a78 \
    --color=marker:#dc8a78,fg+:#4c4f69,prompt:#8839ef,hl+:#d20f39"
 
    # Bat
