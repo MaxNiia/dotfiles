4f97e1749e55b07247e3f684c184757b3943f358
Max Niia <max.niia@epiroc.com>
2025-05-07 07:08:09 +0200

    feat: install wezterm

--- scripts/install/install.sh
+++ scripts/install/install.sh
@@ -55,3 +55,8 @@ fi
 source "$CONFIG/nvim/scripts/init.sh"
 # shellcheck source=../../.config/helix/script/init.sh
 # source "$CONFIG/helix/scripts/init.sh"
+
+curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
+echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
+sudo chmod 644 /usr/share/keyrings/wezterm-fury.gpg
+apt_install wezterm-nightly
